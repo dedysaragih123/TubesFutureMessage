27c688700182fd1fb72e30af4808f7db0b698f75
dedysaragih123 <118517967+dedysaragih123@users.noreply.github.com>
2025-01-11 18:12:13 +0700

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install -e backend
-python3 -m gunicorn app:app -c src/gunicorn.conf.py
+python3 -m gunicorn app.main:app -c src/gunicorn.conf.py
