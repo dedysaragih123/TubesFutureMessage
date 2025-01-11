de41af4f70109ba0d7ac97b36cc32dd5b3da186d
dedysaragih123 <118517967+dedysaragih123@users.noreply.github.com>
2025-01-11 17:52:48 +0700

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 python3 -m pip install --upgrade pip
-python3 -m pip install -r backend/requirements.txt
+python3 -m pip install -e backend
 exec gunicorn backend/app.main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
