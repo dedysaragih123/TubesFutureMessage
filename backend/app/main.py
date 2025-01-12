1421a6ed7c88837d97b8c032bbd41534890acf93
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2025-01-13 02:04:25 +0700

    push coba2

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
                    "https://future-message-caexg0bxeha9d2et.southeastasia-01.azurewebsites.net",
-                   "https://tubes-future-message.vercel.app/",],  # Ganti dengan domain deploy
+                   "https://tubes-future-message.vercel.app",],  # Ganti dengan domain deploy
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
